8f0bc4aa9927a15f8bb4df690f46b17d5652b89e
richardmazimpk <richardmazimpk@gmail.com>
2026-03-25 18:45:13 +0200

    Add files via upload

---
+++ Example_0ne_if_condition_Python.py
@@ -0,0 +1,110 @@
+
+# Online Exam Simulation Program
+
+# Registered = input("Are you registered for Exam: (yes/No) : "")
+# school_fees = input("Are the Tuition paid already?: Yes/No: "")
+
+# if Registered == "Yes" and school_fees == "Yes" :
+# print("You're cleared")
+#       elif Registered == "Yes" and school_fees == "No":
+#       print("not Allowed into the Exam")
+#       elif Registered == "No" and Tution_fee == "yes":
+#       print("Not"Allowed into the Exam2)
+#       else:
+#       print("Not Allowed to come to school"\ "")
+# Number = float(input("Enter the Number of your Choice:""))
+# if number > 0:
+#             print("The Number is Poitive!"")
+#             elif number < 0:
+#             print("" The Number is Negative!"")
+#             else:
+#             print("The Number is Neutral")
+
+# Grading System Simulation
+
+# score = input("enter your score")
+# if score >= 80:
+#     print(This is Grade A)
+# if score >= 70:
+#     print(This is Grade B)
+# if score >= 60:
+#     print(This is Grade C)
+# if score >50:
+#     print(This is Grade D)
+# else:
+#     print("You have failed")
+
+# Salary simulation program
+
+# new_salary = float(input("Enter the salary: "))
+# Experience = int (input("Enter the Experience: "))
+
+# if Experience>=10:
+#     new_salary = new_salary + (0.5*new_salary)
+#     print(f"your new new_salary is {new_salary}rwf")
+# elif Experience>=5:
+#     new_salary =new_new_salary+ (0.2*new_salary)
+#     print(f"your new_salary is {new_salary}rwf")
+# else:
+
+#     print(f"your new_salary is{new_salary}rwf")
+
+
+# Days Of The Week
+
+# Day= input("Enter the day: ")
+# match Day:
+#     case "1":
+#         print("Monday")
+#     case "2":
+#         print("Tuesday")
+#     case _:
+#         print("{error}")
+
+# Displaying days of the week
+
+# Day = input("Enter the day of the week: ")
+# match Day:
+#     case "Monday":
+#         print("This is the first Day")
+#     case "Tuesday":
+#         print("This is the second Day")
+#     case "Wednesday":
+#         print("This is the third Day")
+#     case "Thursday":
+#         print("This is fourth Day")
+#     case "Friday":
+#         print("This is the fifth Day")
+#     case "Sartuday":
+#         print("This is sixth Day")
+#     case"Sunday":
+#         print("Thisn is the seventh Day")
+#     case _:
+#         print("You have entered the invalidinput")
+      
+        
+# Menu similation
+
+Restraunts = input("Enter the items")
+match Food_items:
+    case "Rice":
+      print(" Serve all type of Rice ")
+    case"Red meat ": 
+       print("Serve Red meat")
+    case " Juice" :
+       print(" serve Mango|apple Juice")
+    case _:
+       print(" You entered the invalid input ")
+
+
+
+
+
+
+     
+
+        
+
+
+
+
