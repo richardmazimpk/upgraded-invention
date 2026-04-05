320c7bcc40f0e402d6a51ffc914862e497bca4f1
richardmazimpk <richardmazimpk@gmail.com>
2026-04-05 12:16:15 +0200

    Refactor AES encryption and GUI functionality
    Refactor AES encryption and decryption functions, fix variable names, and improve GUI layout.

--- Advanced Encryption Standard (AES).py
+++ Advanced Encryption Standard (AES).py
@@ -1,26 +1,3 @@
-#
-
-#     functions
-
-#     Example_ two
-
-# def my_function(x,y):
-#         return x*y
-    
-# x = int(input("Enter the First Number:"))
-# y = int(input("Enter the second Number :"))
-
-# result = my_function(x,y)
-# print("Result: ", result)
-
-# Example 2
-
-# def myfunction(name, age):
-#     print(f"Hello {name}. You're {age}Years Old")
-
-# myfunction("liam" ,30)   
-# myfunction("jenny" , 50)
-# myfunction("luke", 70) 
 
 import tkinter as tk
 from tkinter import messagebox
